f93cafee429284e93790374c421003d222f976ce
bytemaestr0 <cipikali7@gmail.com>
2025-10-14 11:24:43 +0300

    headers2

--- src/services/videoSources.js
+++ src/services/videoSources.js
@@ -4,12 +4,12 @@ export const videoSources = {
     
     return {
       vidsrc: type === 'movie'
-        ? `https://vidsrc.to/embed/movie/${tmdbId}`
-        : `https://vidsrc.to/embed/tv/${tmdbId}/${season}/${episode}`,
+        ? `https://vidsrc.to/embed/movie/${tmdbId}?autoplay=1&no-ads=true`
+        : `https://vidsrc.to/embed/tv/${tmdbId}/${season}/${episode}?autoplay=1&no-ads=true`,
 
       vidlink: type === 'movie'
-        ? `https://vidlink.pro/movie/${tmdbId}`
-        : `https://vidlink.pro/tv/${tmdbId}/${season}/${episode}`,
+        ? `https://vidlink.pro/movie/${tmdbId}?minimal=1`
+        : `https://vidlink.pro/tv/${tmdbId}/${season}/${episode}?minimal=1`,
 
       superembed: type === 'movie'
         ? `https://multiembed.mov/?video_id=${tmdbId}&tmdb=1`
